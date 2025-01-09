879cddcf8972e4b8bc4c49cc8e906c75bdeb0b1e
CommanderChubbins <nmdm.ca@gmail.com>
2025-01-09 09:58:19 -0800

    Update sketch_de16a.ino

--- sketch_de16a.ino
+++ sketch_de16a.ino
@@ -3,10 +3,13 @@
 #define SENSOR_MIN 0
 #define SENSOR_MAX 1023
 
+int sensor0 = analogRead(A0); 
+int sensor1 = analogRead(A1);   
+
 Servo servo0;
 int servopin = 9;
 
-int light_direction(int sensor0, int sensor1)
+int light_direction(void)
 {
   if (sensor0 > (sensor1 + 5))
   {    
@@ -25,8 +28,15 @@ int light_direction(int sensor0, int sensor1)
   }
 }
 
-void servo_move(bool start) {
-  //int current = servo0.read();
+void servo_move(void) {
+  int current = servo0.read();
+  int dr = 1;
+  while (int direction = light_direction()))
+  {
+    servo0.write(current + dr);
+    dr++
+    current = servo0.read();
+  }
   //servo0.write(current);
   //servo0.write(0);
 
@@ -34,16 +44,12 @@ void servo_move(bool start) {
 
 void setup() {
   Serial.begin(9600);
-  // Serial1.begin(9600);
+  //Serial1.begin(9600);
   servo0.attach(servopin);
 }
 
 void loop() {
   // Reads light value from sensor on pin A0 every 1000ms (1s)
-  // Prints raw input value and mapped (0 to 5) value  
-  int sensor0 = analogRead(A0); 
-
-  int sensor1 = analogRead(A1);   
   
   Serial.print("raw sensor0: ");
   Serial.println(sensor0);
