2ba14f0c3ace48944cf617d6da7eee3a2ad92a45
CommanderChubbins <nmdm.ca@gmail.com>
2025-01-15 09:58:31 -0800

    Update sketch_de16a.ino
    added averaging sensor readings 
    added table of sensor difference to angle

--- sketch_de16a.ino
+++ sketch_de16a.ino
@@ -1,117 +1,137 @@
-#include <EEPROM.h>
-#include <Servo.h>
+  #include <EEPROM.h>
+  #include <Servo.h>
 
-#define SENSOR_MIN 0
-#define SENSOR_MAX 1023
+  #define SENSOR_MIN 0
+  #define SENSOR_MAX 1023
+  #define RANGES 3
+  //int avgsensor0 = analogRead(A0);
+  //int avgsensor1 = analogRead(A1);  
 
-int sensor0 = analogRead(A0);
-int sensor1 = analogRead(A1);  
+  // int pwradr = 0;
 
-// int pwradr = 0;
-
-int centerswitch = 5;
-
-Servo servo0;
-int servopin = 9;
-
-int resistor = 5;
-
-int time;
-
-void power_calc(void)
-{
-  // total energy generated since powered on 
-  int volts = analogRead(A2);
-  float power = (volts * volts) / resistor;
-  time = millis();
-
-  float energy = power * time;
-  // EEPROM.put(pwradr, energy); 
-}
-
-int light_direction(void)
-{
-  // average sensors ~5 times 
-  sensor0 = analogRead(A0);
-  sensor1 = analogRead(A1);
-  
-  // change angle based on difference in sensor readings  
-  if (sensor0 > (sensor1 + 10))
-  {    
-    // move towards sensor0
-    //Serial.println(sensor0);
-    //Serial.println(sensor1 + 10);
-    return -1;
+  // max, angle 
+  int difftoangle[RANGES][2] = {
+    {10, 1},
+    {30, 2},
+    {50, 4}
   }
-  else if (sensor0 < (sensor1 - 10))
+
+  int centerswitch = 7;
+
+  Servo servo0;
+  int servopin = 9;
+
+  int resistor = 5;
+
+  int time;
+
+  void power_calc(void)
   {
-    // move towards sensor1
-    //Serial.println(sensor1);
-    return 1;
-  }
-  else
-  {
-    // don't move
-    //Serial.print(sensor0);
-    //Serial.println(sensor1);
-    return 0;
-  }
-}
+    // total energy generated since powered on 
+    int volts = analogRead(A2);
+    float power = (volts * volts) / resistor;
+    time = millis();
 
-void servo_move(int direction) {
-  // move towards sensor while sensors are different 
-  int current = servo0.read();
-  
-  //Serial.println(servo0.read());
-  int dr = current + (direction * 4);
-
-  if (dr > 10 && dr < 170)
-  {
-    servo0.write(dr);
-  }
-  
-  //while (int direction = light_direction())
-  
-}
-
-void setup() {
-  Serial.begin(9600);
-  servo0.attach(servopin);
-  pinMode(centerswitch, INPUT);
-}
-
-void loop() {
-  // Reads light value from sensor on pin A0 every 1000ms (1s)
-  
-  //Serial.print("raw sensor0: ");
-  //Serial.println(sensor0);
-  //Serial.print("raw sensor1: ");
-  //Serial.println(sensor1);
-  
-  // servo_move();
-  /*float saved;
-  EEPROM.get(pwradr, saved);
-  Serial.println(saved);
-  */
-  //power_calc();
-  // center motor and cancel movement if switch is on 
-  int count = Serial.available();
-  if (count)
-  {
-    Serial.print("recieved: ");
-    Serial.println(Serial.readString());
+    float energy = power * time;
+    // EEPROM.put(pwradr, energy); 
   }
 
-  
-  if (digitalRead(centerswitch))
+  int light_direction(void)
   {
-    servo0.write(90);
+    // average sensors 5 times 
+    // add input sample amount & delay
+    int avgsensor0 = 0;
+    int avgsensor1 = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+      avgsensor0 += analogRead(A0);
+      avgsensor1 += analogRead(A1);
+      delay(200);
+    }
+    avgsensor0 /= 5;
+    avgsensor1 /= 5;
+
+    int diff = abs(avgsensor0 - avgsensor1);
+
+    // search table for angle based on difference 
+    for (int i = 0; i < RANGES; ++i)
+    {
+      // greater than maximum for an angle 
+      if (diff > difftoangle[i][0])
+      {
+        // wrong pair
+      }
+      else
+      {
+        // right pair
+      }
+    }
+    #if 0
+    // change angle based on difference in sensor readings  
+    if (avgsensor0 > (avgsensor1 + 10))
+    {    
+      // move towards avgsensor0
+      //Serial.println(avgsensor0);
+      //Serial.println(avgsensor1 + 10);
+      return -1;
+    }
+    else if (avgsensor0 < (avgsensor1 - 10))
+    {
+      // move towards avgsensor1
+      //Serial.println(avgsensor1);
+      return 1;
+    }
+    else
+    {
+      // don't move
+      //Serial.print(avgsensor0);
+      //Serial.println(avgsensor1);
+      return 0;
+    }
+    #endif
+
   }
-  // figure out if light direction is "found" or stable 
-  // then, if a sudden change occurs, pause for n time before checking and moving   again
-  else if (int direction = light_direction())
-  {
-    servo_move(direction);
+
+  void servo_move(int direction) {
+    // move motor towards sensors
+    int current = servo0.read();
+    int dr = current + (direction * 4);
+
+    if (dr > 10 && dr < 170)
+    {
+      servo0.write(dr);
+    }
   }
-  delay(500);
-}
+
+  void setup() {
+    Serial.begin(9600);
+    servo0.attach(servopin);
+    pinMode(centerswitch, INPUT);
+  }
+
+  void loop() {
+    /*float saved;
+    EEPROM.get(pwradr, saved);
+    Serial.println(saved);
+    */
+
+    int count = Serial.available();
+    if (count)
+    {
+      Serial.print("recieved: ");
+      Serial.println(Serial.readString());
+    }
+
+    // center motor and cancel movement if switch is on 
+    if (digitalRead(centerswitch))
+    {
+      servo0.write(90);
+    }
+    // figure out if light direction is "found" or stable 
+    // then, if a sudden change occurs, pause for n time before checking and moving   again
+    else if (int direction = light_direction())
+    {
+      servo_move(direction);
+    }
+    delay(500);
+  }
